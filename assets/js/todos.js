9dd6134cca60ddf6034bb063aa3301628341c374
bishalraktim <raktimbishal@gmail.com>
2020-04-23 22:37:58 +1000

    Initial commit

---
+++ assets/js/todos.js
@@ -0,0 +1,27 @@
+$("ul").on("click", "li", function(){
+  $(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(event){
+  event.stopPropagation();
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  });
+});
+
+$("input[type='text']").keypress(function(e){
+  if($(this).val() === ""){
+    $(this).prepend();
+  }
+  else{
+    if(e.which === 13){
+      var inputText = $(this).val();
+      $(this).val("");
+      $("ul").append("<li><span class='trash'><i class='far fa-trash-alt'></i></span> " + inputText + "</li>");
+    }
+  }
+});
+
+$(".shift").click(function(){
+  $("input[type='text']").fadeToggle();
+})
