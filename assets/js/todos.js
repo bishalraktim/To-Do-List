5b4703b8dd51fe8d4ceb05bc462b86a74999c9e4
bishalraktim <raktimbishal@gmail.com>
2020-04-24 23:20:52 +1000

    Fix mobile transition effect

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,34 +1,35 @@
-$("ul").on("click", "li", function(){
-  $(this).toggleClass("completed");
-});
+$(document).ready(function(){
 
-$("ul").on("click", "span", function(event){
-  event.stopPropagation();
-  $(this).parent().fadeOut(500, function(){
-    $(this).remove();
+  $("ul").on("click", "li", function(e){
+    e.preventDefault();
+    $(this).toggleClass("completed");
   });
-});
 
-$("input[type='text']").keypress(function(e){
-  if($(this).val() === ""){
-    $(this).prepend();
-  }
-  else{
-    if(e.which === 13){
-      var inputText = $(this).val();
-      $(this).val("");
-      $("ul").append("<li><span class='trash'><i class='far fa-trash-alt'></i></span> " + inputText + "</li>");
+  $("ul").on("click", "span", function(event){
+    event.preventDefault();
+    event.stopPropagation();
+    $(this).parent().fadeOut(500, function(){
+      $(this).remove();
+    });
+  });
+
+  $("input[type='text']").keypress(function(e){
+    e.preventDefault();
+    if($(this).val() === ""){
+      $(this).prepend();
     }
-  }
-});
-
-$(".shift").click(function(){
-  $("input[type='text']").fadeToggle();
-});
-
-$("ul").on("mouseout", "li", function(){
-  (".trash").css({
-    "width": 0,
-    "background-color": "white"
+    else{
+      if(e.which === 13){
+        var inputText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span class='trash'><i class='far fa-trash-alt'></i></span> " + inputText + "</li>");
+      }
+    }
   });
+
+  $(".shift").click(function(e){
+    e.preventDefault();
+    $("input[type='text']").fadeToggle();
+  });
+
 });
